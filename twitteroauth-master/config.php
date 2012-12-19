2c92c5527572a63469af028eea47bfa0e3044346
Stephen Tvedt <your@email.com>
2012-12-19 09:26:34 -0500

    initial commit with functional twitter auth

---
+++ twitteroauth-master/config.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @file
+ * A single location to store configuration.
+ */
+
+define('CONSUMER_KEY', 'VJsPHJUChSHgNAOsnICTA');
+define('CONSUMER_SECRET', 'bGNdB6hLKjMH1hYoIlrCZfVIWADggyePlVIUFMXYWc');
+define('OAUTH_CALLBACK', 'http://stephentvedt.com/examples/twitter-app/twitteroauth-master/callback.php
+');
